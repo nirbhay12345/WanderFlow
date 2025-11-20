6e6d45284ffea3fb4eca7c83d7a65c781e62ad3e
nirbhay12345 <nirc34@gmail.com>
2025-11-20 07:56:49 +0000

    remove redundancy from the dlt pipeline

--- wanderline/transformations/reviews_silver.sql
+++ wanderline/transformations/reviews_silver.sql
@@ -2,10 +2,10 @@ CREATE OR REFRESH STREAMING TABLE valid_booking_reviews AS
 SELECT
   br.*
 FROM STREAM(booking_reviews) br
-JOIN booking_info_active bia
-  ON br.booking_id = bia.booking_id
-   AND br.user_id = bia.user_id
-   AND br.property_id = bia.property_id
-   AND bia.check_in < br.updated_at
-WHERE bia.status in ('confirmed', 'completed')
+JOIN booking_info bi
+  ON br.booking_id = bi.booking_id
+   AND br.user_id = bi.user_id
+   AND br.property_id = bi.property_id
+   AND bi.check_in < br.updated_at
+WHERE bi.status in ('confirmed', 'completed') AND bi.__END_AT IS NULL
 ;
